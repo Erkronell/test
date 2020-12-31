5d8f3d589d8002fa8f1ec5149ff6e36ce898c4d7
Erkronell <75565027+Erkronell@users.noreply.github.com>
2020-12-31 12:42:27 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
-$(".slider").bxSlider({
-  mode: "fade",
-  auto: true,
-  pager: false,
+$(document).ready(function () {
+  $(".slider").bxSlider({
+    mode: "fade",
+    auto: true,
+    pager: false,
+  });
 });
+$(document).ready(function () {
+  $(".slider2").bxSlider({
+    slideWidth: 200,
+    slideMargin: 15,
+    minSlides: 5,
+    maxSlides: 8,
+    infiniteLoop: true,
+    controls: false,
+    pager: false,
+    shrinkItems: true,
+  });
 });
